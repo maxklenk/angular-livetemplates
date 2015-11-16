4533fa0d66050c4e2ea5b91b12c2d0aff9aaafd5
Sebastian Henneberg <github@sebastianhenneberg.de>
2015-11-16 11:54:26 +0100

    added controller template utilizing controllerAs

---
+++ ngct.tmpl.js
@@ -0,0 +1,15 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module('$moduleName$')
+    .controller('$controllerName$', $controllerName$);
+
+  function $controllerName$($injectables$) {
+    var vm = this;
+    
+    $END$
+  }
+
+})();
